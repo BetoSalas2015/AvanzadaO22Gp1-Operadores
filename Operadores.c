baf8df18935f5c7f188e63424e1b36989066a1a2
Roberto Salazar Márquez <roberto.salazar@upaep.mx>
2022-09-08 12:47:03 -0500

    Sesión 11 - 08/09/2022

--- Operadores.c
+++ Operadores.c
@@ -1,17 +1,66 @@
+// Sesión 11 - 08/09/2022
 #include <stdio.h>		// Para printf() y scanf()
 #include <stdlib.h>		// Para system()
+#include "defs.h"
 
-//#define macro valor 
-#define pausa system("pause")
-#define cls system("cls")
+void bases()
+{
+	// conversion entre bases
+	a = 179;				// Base 10
+	printf("179 en base 10 equivale a %o base 8\n", a);
+	printf("179 en base 10 equivale a %X base 16\n", a);
 
-#define PR(fmt,val) printf(#val " = %"#fmt" \t", (val))
-#define NL putchar('\n')
+	a = 0263;				// Base 8
+	printf("263 en base 8 equivale a %d base 10\n", a);
+	printf("263 en base 8 equivale a %X base 16\n", a);
+
+	a = 0xB3;				// Base 16 (Hexadecimal)			
+	printf("B3 en base 16 equivale a %d base 10\n", a);
+	printf("B3 en base 16 equivale a %o base 8\n", a);
+
+}
+
+void primarios()
+{
+
+//  Jerarquía Nivel 1 - Operadores Primarios
+	//  Operador llamada a función ( () )
+	printf("Toma un numero aleatorio:  %d\n", rand() );
+
+	PRINT1(f, 5 / 9.0 * 80 - 32 );			//  Error
+	PRINT1(f, 5 / 9.0 * (80 - 32) );		//  Correcto
+	PRINT1(d, (2 + 2) - (3 + 3) );
+
+	// Operador Indice de Arreglo ( [] ) 
+	Arreglo[4] = 20;
+	PRINT1(d, Arreglo[4]);
+
+	//  Operador Punto (Parte de una estructura) ( . )
+	Juanito.edad = 30;
+	PRINT1(d, Juanito.edad);
+
+	//  Operador Flecha (Parte de una estructura) ( -> )
+	ptrJuanito = &Juanito;
+	PRINT1(d, ptrJuanito->edad);
+}
 
 int main()
 {
-	int a = 17;
-	PR(d,5 * 5); NL;
+	// Jerarquía nivel 2 - Operadores unarios
+	//  Operador negación ( ! )
+	a = 0;		//  a = falso
+	if( a )
+		printf("Verdadero\n");
+	else
+		printf("falso\n");
+
+	PRINT2(d, a, !a);
+	PRINT1(d, !!!!!!!!!!!!!!!!a);
+
+	//  Operador Menos unario ( - )
+	a = -3; b = 3;
+	PRINT2(d, a, b);
+
 	pausa;
 	return 0;
 }
---
+++ defs.h
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+//#define macro valor 
+#define pausa system("pause")
+#define cls system("cls")
+
+#define PR(fmt,val) printf(#val " = %"#fmt" \t", (val))
+#define NL putchar('\n')
+
+#define PRINT1(f, x1) PR(f, x1), NL
+#define PRINT2(f, x1, x2) PR(f, x1), PRINT1(f, x2)
+#define PRINT3(f, x1, x2, x3) PR(f, x1), PRINT2(f, x2, x3)
+#define PRINT4(f, x1, x2, x3, x4) PR(f, x1), PRINT3(f, x2, x3, x4)
+#define PRINT5(f, x1, x2, x3, x4, x5) PR(f, x1), PRINT4(f, x2, x3, x4, x5)
+
+// Declaraciones Globales (externas)
+int Arreglo[15];
+
+struct Persona
+{
+	char nombre[30];
+	int edad;
+} Juanito, *ptrJuanito;
+
+char car;
+int a, b, c, d;
+float e , f, g;
+double h;
+int *ptr;
